cfceb2329e069683f5eceec6c9a416da59ca21e9
Alyson <alysonfranca96@gmail.com>
2025-05-04 21:01:12 -0300

    Projeto de Extensão - Extrack (ajustes)

--- project/server/index.js
+++ project/server/index.js
@@ -5,6 +5,9 @@ import connectDB from './config/db.js'
 import authRoutes from './routes/auth.js'
 import projectRoutes from './routes/project.js'
 import { auth } from './middleware/auth.js'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
 
 dotenv.config()
 
@@ -20,6 +23,16 @@ app.use('/api/projects', projectRoutes)
 
 app.get('/api/protected', auth, (req, res) => {
   res.json({ message: 'Rota protegida acessada com sucesso' })
+})
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+const distPath = path.resolve(__dirname, '../dist')
+
+app.use(express.static(distPath))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(distPath, 'index.html'))
 })
 
 const PORT = process.env.PORT || 5000
